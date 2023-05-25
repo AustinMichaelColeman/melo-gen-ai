715069fd92b1918b9fbb0b3bf20859ee1794b0a2
Austin Coleman <AustinMichaelColeman@gmail.com>
2023-05-25 11:06:30 -0700

    Remove express, only using vercel

--- src/server.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-import app from "./app.js";
-
-const PORT = 3000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
-});
