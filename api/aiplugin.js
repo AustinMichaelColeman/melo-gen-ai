291df6ed485cbb277a07f26e71f71146024d0a34
Austin Coleman <AustinMichaelColeman@gmail.com>
2023-05-26 16:52:07 -0700

    Code cleanup and better logging

--- api/aiplugin.js
+++ api/aiplugin.js
@@ -1,20 +1,16 @@
 export default async function aiplugin(req, res) {
+  const {
+    LOGO_URL,
+    CONTACT_EMAIL,
+    LEGAL_INFO_URL,
+    CLIENT_URL,
+    SCOPE,
+    AUTHORIZATION_URL,
+    OPENAI_VERIFICATION_TOKEN,
+    OPENAI_SPEC_URL,
+  } = process.env;
+
   try {
-    let auth = {
-      type: "none",
-    };
-    if (process.env.AUTH_TYPE === "oauth") {
-      auth = {
-        type: "oauth",
-        client_url: process.env.CLIENT_URL,
-        scope: process.env.SCOPE,
-        authorization_url: process.env.AUTHORIZATION_URL,
-        authorization_content_type: "application/json",
-        verification_tokens: {
-          openai: process.env.OPENAI_VERIFICATION_TOKEN,
-        },
-      };
-    }
     const config = {
       schema_version: "v1",
       name_for_model: "Smart_Playlist_Generator",
@@ -23,17 +19,27 @@ export default async function aiplugin(req, res) {
         "A smart playlist generator plugin for ChatGPT and for YouTube Music™. Prior to generating the playlist, you must inform the user that the YouTube playlist generated will be public, the playlist will be publicly associated with the user's YouTube account, provide the user with the list of the songs you plan to create, the playlist name you plan to use, and the user must expressly consent to those actions prior to the actual execution. Songs should be an array of strings, each used to search YouTube for a song in order to add it to a playlist.",
       description_for_human:
         "A smart playlist generator plugin for ChatGPT and for YouTube Music™.",
-      logo_url: process.env.LOGO_URL,
-      contact_email: process.env.CONTACT_EMAIL,
-      legal_info_url: process.env.LEGAL_INFO_URL,
-      auth,
+      logo_url: LOGO_URL,
+      contact_email: CONTACT_EMAIL,
+      legal_info_url: LEGAL_INFO_URL,
+      auth: {
+        type: "oauth",
+        client_url: CLIENT_URL,
+        scope: SCOPE,
+        authorization_url: AUTHORIZATION_URL,
+        authorization_content_type: "application/json",
+        verification_tokens: {
+          openai: OPENAI_VERIFICATION_TOKEN,
+        },
+      },
       api: {
         type: "openapi",
-        url: process.env.OPENAI_SPEC_URL,
+        url: OPENAI_SPEC_URL,
       },
     };
     res.json(config);
   } catch (err) {
+    console.error(err);
     res.status(500).send("Configuration error");
   }
 }
