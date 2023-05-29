5d336929bfca8de8621f1aeeafadd9b68d7e6916
Austin Coleman <AustinMichaelColeman@gmail.com>
2023-05-28 21:33:14 -0700

    handle errors in case server config does not exist

--- api/openapi.js
+++ api/openapi.js
@@ -2,11 +2,16 @@ import path from "path";
 import fs from "fs";
 
 export default async function openapi(req, res) {
-  const filePath = path.resolve("./config/openapi.yaml");
+  try {
+    const filePath = path.resolve("./config/openapi.yaml");
 
-  let openapiYaml = await fs.promises.readFile(filePath, "utf8");
-  openapiYaml = openapiYaml.replace("__SERVER_URL__", process.env.SERVER_URL);
+    let openapiYaml = await fs.promises.readFile(filePath, "utf8");
+    openapiYaml = openapiYaml.replace("__SERVER_URL__", process.env.SERVER_URL);
 
-  res.setHeader("Content-Type", "text/yaml");
-  res.send(openapiYaml);
+    res.setHeader("Content-Type", "text/yaml");
+    res.send(openapiYaml);
+  } catch (error) {
+    console.error("Error in openapi:", error);
+    res.status(500).json({ error: "Failed to load OpenAPI specification" });
+  }
 }
