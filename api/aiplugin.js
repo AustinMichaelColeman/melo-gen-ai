36584ee66917e84824107f9cffc05af91d5e1a8f
Austin Coleman <AustinMichaelColeman@gmail.com>
2023-05-29 14:51:14 -0700

    Clarify description

--- api/aiplugin.js
+++ api/aiplugin.js
@@ -7,8 +7,16 @@ export default async function aiplugin(req, res) {
       schema_version: "v1",
       name_for_model: "MeloGenAI",
       name_for_human: "MeloGenAI",
-      description_for_model:
-        "A playlist generator plugin for ChatGPT and for YouTube Music™. Prior to generating the playlist, you must inform the user that the YouTube playlist generated will be public, the playlist will be publicly associated with the user's YouTube account, provide the user with the list of the song search queries you plan to use, inform the user the way the plugin works is by searching for each query and adding the first result to the playlist, so it may not always be accurate, the playlist name you plan to use, and the user must expressly consent to those actions prior to the actual execution. Songs should be an array of strings, each used to search YouTube for a song in order to add it to a playlist.",
+      description_for_model: `A playlist generator plugin for ChatGPT and for YouTube Music™. Prior to generating the playlist, you must inform the user:
+        
+        * that the YouTube playlist generated will be public
+        * that the YouTube playlist will be associated with the user's account
+        * that the user can revoke the permissions at any time at https://myaccount.google.com/permissions
+        * that the plugin works by searching YouTube for each song title and adding the first result to the playlist, so it may not always be accurate
+        
+        You will also need this information:
+        * the name of the playlist, which you can suggest or allow the user to provide.
+        * the list of songs names to search in order to add to the playlist.`,
       description_for_human:
         "A playlist generator plugin for ChatGPT and for YouTube Music™",
       logo_url: `${SERVER_URL}/logo.png`,
