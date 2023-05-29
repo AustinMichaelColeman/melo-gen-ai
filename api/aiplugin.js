cfb63bc6b7a4b5b389f5dd5d29c1cfe8d44cf2dd
Austin Coleman <AustinMichaelColeman@gmail.com>
2023-05-29 15:09:01 -0700

    Update descriptions

--- api/aiplugin.js
+++ api/aiplugin.js
@@ -7,16 +7,18 @@ export default async function aiplugin(req, res) {
       schema_version: "v1",
       name_for_model: "MeloGenAI",
       name_for_human: "MeloGenAI",
-      description_for_model: `A playlist generator plugin for ChatGPT and for YouTube Music™. Prior to generating the playlist, you must inform the user:
-        
-        * that the YouTube playlist generated will be public
-        * that the YouTube playlist will be associated with the user's account
-        * that the user can revoke the permissions at any time at https://myaccount.google.com/permissions
-        * that the plugin works by searching YouTube for each song title and adding the first result to the playlist, so it may not always be accurate
-        
-        You will also need this information:
-        * the name of the playlist, which you can suggest or allow the user to provide.
-        * the list of songs names to search in order to add to the playlist.`,
+      description_for_model: `MeloGenAI is a playlist generator plugin for ChatGPT and YouTube Music™. It creates a public YouTube playlist associated with your YouTube Music account based on a list of song titles you provide. 
+
+Please note:
+- The YouTube playlist generated will be public.
+- The YouTube playlist will be associated with your YouTube Music account.
+- You can revoke the permissions at any time at https://myaccount.google.com/permissions.
+- The plugin works by searching YouTube for each song title and adding the first result to the playlist. The accuracy of the search results may vary.
+
+To generate a playlist, the plugin will request from the user:
+- The name of the playlist.
+- The list of song titles to search and add to the playlist.`,
+
       description_for_human:
         "A playlist generator plugin for ChatGPT and for YouTube Music™",
       logo_url: `${SERVER_URL}/logo.png`,
