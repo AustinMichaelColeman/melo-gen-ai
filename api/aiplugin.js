fbc20bd952307f59f1f8db29abdecabd2d0c3acd
Austin Coleman <AustinMichaelColeman@gmail.com>
2023-05-30 03:53:06 -0700

    Update plugin description

--- api/aiplugin.js
+++ api/aiplugin.js
@@ -8,7 +8,7 @@ export default async function aiplugin(req, res) {
       name_for_model: "MeloGenAI",
       name_for_human: "MeloGenAI",
       description_for_model:
-        "Generate playlists for YouTube Music. Explicit informed consent is required prior to generating the playlist.",
+        "Generate playlists for YouTube Music. Explicit informed consent is required prior to generating the playlist, including the list of song search queries, playlist title, and playlist privacy setting.",
       description_for_human: "Generate playlists for YouTube Music™.",
       logo_url: `${SERVER_URL}/logo.png`,
       contact_email: CONTACT_EMAIL,
